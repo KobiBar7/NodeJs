940d764d28483b56cced4616bec78d6c37528847
Your Name <your@email.com>
2023-01-04 19:19:50 +0200

    first commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const cors = require('cors');
 
 
 app.listen(port, () => {
@@ -9,12 +10,9 @@ app.listen(port, () => {
 })
 
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Origin", "Origin, X-requestd-With, Context-Type, Accept");
-  next();
-})
-
+app.use(cors({
+  origin: '*'
+}));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
