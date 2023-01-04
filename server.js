5198f48e09a0c88b0ddf95d1eba67b09ac2c7db9
Your Name <your@email.com>
2023-01-04 19:42:53 +0200

    first commit

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const port = 5000
 const cors = require('cors');
 
 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
