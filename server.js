6f30d7cdbeeba7094fdcc85659ca12bada6da9fd
Your Name <your@email.com>
2023-01-04 17:39:16 +0200

    first commit

--- server.js
+++ server.js
@@ -2,10 +2,57 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+
+
+
+app.get('/Test', function (req, res) {
+  var listResultObJ = "okkkkkkkkkk";
+  res.send(listResultObJ);
+})
+
+
+app.post('/SendEmail', function (req, res) { 
+  var transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'testeee777@gmail.com',
+        pass: 'ulananlpkmktdtcb'
+      }
+    });
+    
+    var reqObj = JSON.parse(req.body.jsonObj);
+
+    var subject = reqObj["name"] + " - "  + reqObj["email"] + " - " + reqObj["address"] + " - " + reqObj["phone"] + " - " + reqObj["subject"];
+    var text = reqObj["message"];
+
+    var mailOptions = {
+      from: 'testeee777@gmail.com',
+      to: 'barkobi57@gmail.com',
+      subject: subject,
+      text: text
+    };
+    
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+        return info;
+      }
+    });
+})
+
+
