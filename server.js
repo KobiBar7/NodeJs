3640aed9d4191b67329c71d30f344b7be372e0b5
Your Name <your@email.com>
2023-01-04 19:07:17 +0200

    first commit

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.listen(port, () => {
 })
 
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "Origin, X-requestd-With, Context-Type, Accept");
+  next();
+})
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
