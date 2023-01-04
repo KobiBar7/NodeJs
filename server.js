46cc2d8dff932477bebbcc521d5d0b34a2be92a0
Your Name <your@email.com>
2023-01-04 20:02:39 +0200

    first commit

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
+
+
 const express = require('express')
-const app = express()
-const port = 5000
+
+const app = express();
 const bodyParser = require('body-parser');
 
 const cors = require('cors');
 
-
+const port = 5000
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
