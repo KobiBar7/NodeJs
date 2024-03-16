d0727901239d49c640713d75521f744250ef1c1b
Your Name <your@email.com>
2024-03-16 20:53:11 +0200

    first commit

--- server.js
+++ server.js
@@ -233,7 +233,7 @@ app.post('/SendEmailArch', function (req, res) {
 
     var reqObj = req.body;
     var subject = reqObj["Name"] + ' - ' + 'ליד חדש';
-    var emailTo = 'mirankatzav@gmail.com'; // 
+    var emailTo = 'dr.miran.k@gmail.com'; // 
     
 
     //var text = reqObj["message"];
